f8cd6eafb6dd0be1b08749290d4b4933f540853e
Manik Ahmed <kormoi.com@gmail.com>
2025-08-02 01:07:17 +0600

    Saving changes before rebase

--- dbtask.js
+++ dbtask.js
@@ -3,12 +3,13 @@ const fncs = require("./function");
 const cstyler = require("cstyler");
 
 let x = [
-      "CREATE TABLE IF NOT EXISTS cart (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, buyer_id BIGINT NULL COMMENT 'Foreign key referencing users table', session_id VARCHAR(255) NULL COMMENT 'Stores session ID for guest users', item_type VARCHAR(100) NOT NULL DEFAULT 'sell_item', item_id BIGINT NOT NULL, currency VARCHAR(5) NOT NULL DEFAULT 'BDT', price FLOAT NOT NULL DEFAULT '0', quantity INT NOT NULL DEFAULT '1', platform_charge FLOAT NOT NULL DEFAULT '0' COMMENT 'Platform charge in percentage.', created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, FOREIGN KEY (buyer_id) REFERENCES users(id) ON DELETE SET NULL) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    "CREATE TABLE IF NOT EXISTS cart (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, buyer_id BIGINT NULL COMMENT 'Foreign key referencing users table', session_id VARCHAR(255) NULL COMMENT 'Stores session ID for guest users', item_type VARCHAR(100) NOT NULL DEFAULT 'sell_item', item_id BIGINT NOT NULL, currency VARCHAR(5) NOT NULL DEFAULT 'BDT', price FLOAT NOT NULL DEFAULT '0', quantity INT NOT NULL DEFAULT '1', platform_charge FLOAT NOT NULL DEFAULT '0' COMMENT 'Platform charge in percentage.', created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, FOREIGN KEY (buyer_id) REFERENCES users(id) ON DELETE SET NULL) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 ]
 
 async function dbTask(config, table_config, availabledatabase) {
     try {
         let insertedjson = {};
+        let errorLog = [];
         for (const databaseName of Object.keys(availabledatabase)) {
             config.database = databaseName.toLocaleLowerCase();
             const dbTableNames = await fncs.getTableNames(config, databaseName.toLocaleLowerCase());
@@ -17,17 +18,32 @@ async function dbTask(config, table_config, availabledatabase) {
             let foreignKeyForLater = [];
             for (const JSONTableName of JSONTableNames) {
                 const isValidTableName = fncs.perseTableNameWithLoop(JSONTableName);
-                let tableName = "";
-                if(fncs.isJsonObject(isValidTableName)){
-                    tableName = isValidTableName.
-                } else {
+                if (isValidTableName === false) {
+                    delete availabledatabase[databaseName][JSONTableName];
+                    errorLog.push(`${cstyler.purpal("Database:")} ${cstyler.blue(databaseName)} > ${cstyler.purpal("Table:")} ${cstyler.blue(JSONTableName)} ${cstyler.red("- Table name is not valid.")}`)
+                    continue;
                 }
-                if (dbTableNames.includes(JSONTableName)) {
+                let tableName = "";
+                if (typeof isValidTableName.loop === "string") {
+                    if (isValidTableName.loop.toLocaleLowerCase() === "year") {
+                        tableName = isValidTableName.name + '_' + new Date().getFullYear();
+                    } else if (isValidTableName.loop.toLocaleLowerCase() === "month") {
+                        tableName = isValidTableName.name + '_' + new Date().getFullYear() + '_' + new Date().getMonth();
+                    } else {
+                        delete availabledatabase[databaseName][JSONTableName];
+                        errorLog.push(`${cstyler.purpal("Database:")} ${cstyler.blue(databaseName)} > ${cstyler.purpal("Table:")} ${cstyler.blue(JSONTableName)} ${cstyler.red("- Table name is not valid.")}`)
+                        continue;
+                    }
+                } else {
+                    tableName = isValidTableName.name;
+                }
+                if (dbTableNames.includes(tableName)) {
                     // alter table
                     // Drop Column
                 } else {
                     // insert table
                     let queryText = `CREATE TABLE IF NOT EXISTS ${JSONTableName.toLocaleLowerCase()} (`;
+                    // add column name
 
 
                     queryText += ") CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
